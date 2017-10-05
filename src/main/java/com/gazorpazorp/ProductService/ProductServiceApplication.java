92b28c193861a228155be214de30797b53332f79
Christopher Peckover <christopher.peckover@gmail.com>
2017-10-04 23:57:45 -0400

    Add kind shutdown

--- src/main/java/com/gazorpazorp/ProductService/ProductServiceApplication.java
+++ src/main/java/com/gazorpazorp/ProductService/ProductServiceApplication.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
@@ -19,6 +23,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import com.gazorpazorp.model.Product;
 import com.gazorpazorp.service.ProductRepositoryCreationService;
 import com.gazorpazorp.service.ProductRepositoryUpdateService;
+import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication(scanBasePackages="com.gazorpazorp")
 @EnableJpaRepositories("com.gazorpazorp.repository")
@@ -62,6 +67,19 @@ public class ProductServiceApplication {
 	        config.exposeIdsFor(Product.class);
 	        config.setBasePath("api");
 	    }
+	}
+	
+	@Bean
+	@Profile("!dev")
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) 
+	{
+		EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+		instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+		instance.setDataCenterInfo(info);
+		instance.setNonSecurePort(8080);
+		return instance;
 	}
 	
 //	@Bean
