a30981d8db083bda192f5bf1afb182c18d39bbb6
xCJPECKOVERx <cjpeckover@hotmail.ca>
2017-10-05 18:13:10 -0400

    Update ProductServiceApplication.java
    Damn maven doesn't think the dependencies for the HSQLDB Manager exist and fail on build. Its stupid.

--- src/main/java/com/gazorpazorp/ProductService/ProductServiceApplication.java
+++ src/main/java/com/gazorpazorp/ProductService/ProductServiceApplication.java
@@ -2,7 +2,6 @@ package com.gazorpazorp.ProductService;
 
 import javax.annotation.PostConstruct;
 
-import org.hsqldb.util.DatabaseManagerSwing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
